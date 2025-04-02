2a45ca8acbcbdf3efad1e4c537977e6aa4750bbc
Pedro Caldas <pedro.caldas009@gmail.com>
2025-04-01 23:21:53 -0300

    apenas os atributos das classes que usam lombok

--- ac20251/src/br/edu/cs/poo/ac/seguro/entidades/Apolice.java
+++ ac20251/src/br/edu/cs/poo/ac/seguro/entidades/Apolice.java
@@ -1,5 +1,10 @@
 package br.edu.cs.poo.ac.seguro.entidades;
 
-public class Apolice {
+import java.math.BigDecimal;
 
+public class Apolice {
+	private Veiculo veiculo;
+	private BigDecimal valorFranquia;
+	private BigDecimal valorPremio;
+	private BigDecimal valorMaximoSegurado;
 }
--- ac20251/src/br/edu/cs/poo/ac/seguro/entidades/Sinistro.java
+++ ac20251/src/br/edu/cs/poo/ac/seguro/entidades/Sinistro.java
@@ -1,5 +1,14 @@
 package br.edu.cs.poo.ac.seguro.entidades;
 
-public class Sinistro {
+import java.time.LocalDateTime;
+import java.math.BigDecimal;
 
+public class Sinistro {
+	private String numero;
+	private Veiculo veiculo;
+	private LocalDateTime dataHoraSinistro;
+	private LocalDateTime dataHoraRegistro;
+	private String usuarioRegistro;
+	private BigDecimal valorSinistro;
+	private TipoSinistro tipo;
 }
