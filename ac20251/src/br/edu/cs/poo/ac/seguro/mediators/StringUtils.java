a7a4535b0c4910c3cd63d138cf787756393ba7b9
Joao Danilo Correia Jota <joaodanilo2006@gmail.com>
2025-04-11 15:49:08 -0300

    string utils

--- ac20251/src/br/edu/cs/poo/ac/seguro/mediators/StringUtils.java
+++ ac20251/src/br/edu/cs/poo/ac/seguro/mediators/StringUtils.java
@@ -3,9 +3,13 @@ package br.edu.cs.poo.ac.seguro.mediators;
 public class StringUtils {
 	private StringUtils() {}
 	public static boolean ehNuloOuBranco(String str) {
-		return false;
-	}
+        return str == null || str.trim().isEmpty();
+    }
+	
     public static boolean temSomenteNumeros(String input) {
-        return false; 
+        if (input == null) {
+            return false;
+        }
+        return input.matches("\\d+");
     }
 }
